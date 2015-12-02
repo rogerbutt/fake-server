63c5c48e11f669e6102d8b0c33c24d08b75f8854
rogerbutt <andrew.luu.93@gmail.com>
2015-12-02 15:07:36 -0600

    typo fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.post('/', function (req, res) {
 var server = app.listen(3000, function() {
 	
 	var host = server.address().address;
-	var port - server.address().portl
+	var port = server.address().port;
 	
 	console.log('Fake server listening at http://%s:%s', host, port);
 	
